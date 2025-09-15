dbc0875ae333537a2cc283b5c38587282c4792ea
BagusKrishna <baguskrishnanditya@gmail.com>
2025-09-15 13:32:41 +0700

    setting routes login n register (as early medical kemarin)

--- project/frontend/src/App.tsx
+++ project/frontend/src/App.tsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import './App.css'
-import './index.css'
+import { Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
+import NotFoundPage from './pages/NotFound';
+import ProtectedRoute from './auth/ProtectedRoute';
+import MainLayout from './layouts/MainLayout'
 
 function App() {
   return (
-    <h1 className="bg-red-500 text-white p-4 rounded-lg">
-  Test Tailwind
-</h1>
+    // Bungkus semua rute dengan UIProvider
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
 
-  )
+      <Routes>
+        {/* Public route */}
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+
+        {/* Protected route */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<MainLayout />}>
+          </Route>
+        </Route>
+
+        {/* Rute untuk halaman yang tidak ditemukan */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ project/frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,23 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/frame/sidebar';
+import Navbar from '../components/frame/navbar';
+
+
+function MainLayout() {
+
+  return (
+    <> {/* Gunakan Fragment agar bisa menampung modal di luar div utama */}
+      <div className="flex flex-col h-screen bg-gray-100">
+        <Navbar />
+        <div className="flex flex-1 overflow-hidden">
+          <Sidebar />
+          <main className="flex-1 p-6 overflow-auto">
+            <Outlet />
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MainLayout;
